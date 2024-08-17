b65671bd62db9ef0569f2fe18c57cd5d922d95c1
Semih BENER <benersemih2434@gmail.com>
2024-08-17 12:36:57 +0300

    EfEntityRepositoryBase'den Türeyen Repository Sınıfımızın Oluşturulması

---
+++ ProgrammersBlog.Data/Concrete/RoleRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ProgrammersBlog.Data.Abstract;
+using ProgrammersBlog.Entities.Concrete;
+using ProgrammersBlog.Shared.Data.Concrete.EntityFramework;
+
+namespace ProgrammersBlog.Data.Concrete
+{
+    public class RoleRepository:EfEntityRepositoryBase<Role>,IRoleRepository
+    {
+        public RoleRepository(DbContext context) : base(context)
+        {
+        }
+    }
+}
